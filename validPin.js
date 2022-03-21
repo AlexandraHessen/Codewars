bd72a50ba012847315275ee2bda006b5ee4fd7ea
AlexandraHessen <xl_energy@mail.ru>
2022-03-21 23:55:26 +0300

    valid pin

---
+++ validPin.js
@@ -0,0 +1,12 @@
+function validatePIN (pin) {
+    if(pin.length===4||pin.length===6){
+        if (Number(pin)){
+            return true
+        } else {
+        return false
+    }
+    } else {
+        return false
+    }
+  //return true or false
+}
